5d0a4e7ac2233701bb9aec9928d7ca3c6fe5ff01
Irving mtz <al2173074687@azc.uam.mx>
2024-10-26 01:42:31 -0600

    agregamos HTML y css, junto con dos botones con funcionalidad en js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
-console.log("Hola mundo :"+ API_KEY)
+const header = document.querySelector("header")
+const sectionPosterMovie = document.querySelector(".poster-movie");
+const sectionFavoriteMovie = document.querySelector(".favorite__movies");
+const sectionBestMovie = document.querySelector(".best__movies");
+const sectionInformationMovie = document.querySelector(".information-movie");
+const sectionSearchMovie = document.querySelector(".search-movie")
+const returnInformationMovie = document.querySelector(".return__icon--information-movie");
+const returnSearchMovie = document.querySelector(".return__icon--search-movie");
+
+
+returnInformationMovie.addEventListener("click",()=>{
+    sectionPosterMovie.classList.toggle('inactive');
+    sectionFavoriteMovie.classList.toggle('inactive');
+    sectionBestMovie.classList.toggle('inactive');
+    sectionInformationMovie.classList.toggle('inactive');
+})
+
+
+returnSearchMovie.addEventListener("click",()=>{
+    
+    sectionPosterMovie.classList.toggle('inactive');
+    sectionFavoriteMovie.classList.toggle('inactive');
+    sectionBestMovie.classList.toggle('inactive');
+    sectionSearchMovie.classList.toggle('inactive');
+    header.classList.toggle('inactive')
+})
